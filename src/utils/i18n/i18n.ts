47da0bdff439ce1cc3c94545ff327f07d82cbe48
William He <contact@williamhe.dev>
2024-12-26 12:48:09 -0800

    feat: create terminal

---
+++ src/utils/i18n/i18n.ts
@@ -0,0 +1,15 @@
+import i18next from 'i18next';
+import enTranslation from './locales/en/translation.json';
+
+i18next.init({
+  fallbackLng: 'en',
+  resources: {
+    en: {
+      translation: enTranslation,
+    },
+  },
+});
+
+const { t } = i18next;
+
+export { i18next, t };
