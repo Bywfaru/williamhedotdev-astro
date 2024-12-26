f255fbb4e6cb53c09c618c238d3a9158e7f27bb3
William He <contact@williamhe.dev>
2024-12-26 14:37:27 -0800

    build(deps): add astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import { fileURLToPath, URL } from 'url';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), icon()],
   vite: {
     resolve: {
       alias: [
